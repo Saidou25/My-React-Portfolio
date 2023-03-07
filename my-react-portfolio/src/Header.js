d9ba6a49c789a37f2592cafd089c124eba043e6b
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-03-07 18:47:34 -0500

    commit

--- my-react-portfolio/src/Header.js
+++ my-react-portfolio/src/Header.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import Navigation from './Navigation'
-import About from './About'
-import Resume from './Resume'
-import Contact from './Contact'
-import Portfolio from './Portfolio'
+import Navigation from './Navigation';
+import About from './About';
+import Resume from './Resume';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+import './Header.css';
 const pageComponents = { About, Portfolio, Resume, Contact };
 
 const pages = Object.keys(pageComponents);
@@ -34,7 +35,7 @@ const Header = () => {
     const handlePageChange = (page) => setCurrentPage(page);
 
     return (
-        <div>
+        <div className='header-navigation'>
             <Navigation currentPage={currentPage} handlePageChange={handlePageChange} pages={pages} />
             {renderPage()}
         </div>
