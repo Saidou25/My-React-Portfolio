86038101e481cc6998cf5153e65f636b5aaeae91
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-03-07 13:40:46 -0500

    header styling

--- my-react-portfolio/src/Navigation.js
+++ my-react-portfolio/src/Navigation.js
@@ -5,28 +5,29 @@ import './Navigation.css';
 function Navigation({ currentPage, handlePageChange, pages }) {
 
   return (
-<div className='main'>
-    <div className='row header'>
-      <div className='col-lg-4 flex myname'><h1>Saidou Monta</h1>
-      </div>
-      <div className='col-lg-8'>
-        <div className="navigation" id='nav'>
-          {pages.map((page) => (
-            <article key={page} className='nav-item'>
-              <a
-                href={`#${page.toLowerCase()}`}
-                onClick={() => handlePageChange(page)}
-                className={currentPage === page ? 'nav-link-active text-dark' : 'nav-link'}
-                id='nav-link'
-              >
-                {page}
-              </a>
-            </article>
-          ))}
+    // <div className='main'>
+      <div className='row navigation-header'>
+        <div className='col-lg-4 flex myname'>
+          <h1>Saidou Monta</h1>
+        </div>
+        <div className='col-lg-8'>
+          <div className="navigation" id='nav'>
+            {pages.map((page) => (
+              <article key={page} className='nav-item'>
+                <a
+                  href={`#${page.toLowerCase()}`}
+                  onClick={() => handlePageChange(page)}
+                  className={currentPage === page ? 'nav-link-active text-dark' : 'nav-link'}
+                  id='nav-link'
+                >
+                  {page}
+                </a>
+              </article>
+            ))}
+          </div>
         </div>
       </div>
-      </div>
-    </div>
+    // </div>
   )
 };
 
