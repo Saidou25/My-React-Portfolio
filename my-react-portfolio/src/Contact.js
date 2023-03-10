7992caee9fc595e097b10d5e72a4f6fb2b979528
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-03-09 21:15:38 -0500

    added emailjs and re-did the form

--- my-react-portfolio/src/Contact.js
+++ my-react-portfolio/src/Contact.js
@@ -1,113 +1,70 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from 'react';
+
+import emailjs from 'emailjs-com';
 import "./Contact.css";
 
 
-function Contact() {
+const SERVICE_ID = 'service_3qy8lbc';
+const TEMPLATE_ID = 'template_s6gfci4';
+const USER_ID = 'RWSohpTYy2zdo_uXO';
 
-  const [yourname, setYourName] = useState("");
-  const [email, setEmail] = useState("");
-  const [comment, setComment] = useState("");
 
-  const validEmail = /^\S+@\S+\.\S+$/;
+const Contact = () => {
+  const form = useRef();
 
-  const handleInputChange = (e) => {
-
-    const { name, value } = e.target;
-
-    if (name === "name") {
-      setYourName(value);
-    }
-    if (name === "email") {
-
-      setEmail(value);
-    }
-    if (name === "comment") {
-      setComment(value);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    if (validEmail.test(email)) {
-
-      alert(`Thank you for your Submission ${yourname}.`);
-
-      setYourName("");
-      setEmail("");
-      setComment("");
-    }
-    else {
-      alert(`You entered an invalid email ${yourname}.`);
-      setEmail("");
-    }
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    form.current.reset()
   };
 
   return (
-    // <div className="contact-container">
-      <div className="row-contact">
-        {/* <div className="formContainer"> */}
-        <div className="col-lg-6 left-box">
-          {/* <div className="mb-5"> */}
-
-          <label className="form-label">
-            Name
-          </label>
+    <div className="row-contact">
+      <div className="col-lg-6 left-box">
+        <form ref={form} onSubmit={sendEmail}>
+          <label className="form-label">Name</label>
           <input
             className="form-control"
-            value={yourname}
-            name="name"
-            onChange={handleInputChange}
+          
             type="text"
-            placeholder="Name"
-          />
-
-          <label className="form-label">
-            email
-          </label>
+            name="user_name"
+            placeholder="Name" />
+          <label className="form-label">Email</label>
           <input
             className="form-control"
-            value={email}
-            name="email"
-            onChange={handleInputChange}
-            type="text"
-            placeholder="example@example.com"
-          />
-
-          <label className="form-label">
-            Comment
-          </label>
+            type="email"
+            name="user_email"
+            placeholder="example@example.com" />
+          <label className="form-label">Message</label>
           <textarea
-            className="form-control comments"
-            value={comment}
-            name="comment"
-            onChange={handleInputChange}
-            type="text"
-            placeholder="Comments"
-          ></textarea>
+            className="form-control message"
+            name="message"
+         
+            placeholder="Comments" />
+          <button className="btn btn-primary"
+            type="submit"
+            value="Send">Submit</button>
+        </form>
+      </div>
 
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={handleFormSubmit}>
-            Submit
-          </button>
-          {/* </div> */}
-        </div>
-
-        <div className="col-lg-6 right-box">
-          <div className="box">
-            <div className="inner">
-              <span>Async</span>
-            </div>
-            <div className="inner">
-              <span>Await</span>
-            </div>
+      <div className="col-lg-6 right-box">
+        <div className="box">
+          <div className="inner">
+            <span>Async</span>
+          </div>
+          <div className="inner">
+            <span>Await</span>
           </div>
         </div>
+
       </div>
-    // </div>
-    // </div >
-  )
+    </div>
+  );
 };
 export default Contact;
