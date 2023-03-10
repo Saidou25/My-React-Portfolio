e251939fd6ed163d36eaaed65878e7e431b3cf88
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-03-09 21:41:10 -0500

    nc

--- my-react-portfolio/src/Contact.js
+++ my-react-portfolio/src/Contact.js
@@ -25,8 +25,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact-container">
-    <div className="row-contact">
+    <div className="row contact">
       <div className="col-lg-6 left-box">
         <form ref={form} onSubmit={sendEmail}>
           <label className="form-label">Name</label>
@@ -62,7 +61,6 @@ const Contact = () => {
           <div className="inner">
             <span>Await</span>
           </div>
-        </div>
         </div>
 
       </div>
