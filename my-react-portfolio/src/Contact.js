8be09c3a5fd995ccb1f90ff300e15c1d5398a300
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-03-17 12:51:43 -0400

    added email validation logic. Form works perfectly

--- my-react-portfolio/src/Contact.js
+++ my-react-portfolio/src/Contact.js
@@ -1,29 +1,29 @@
 import React, { useRef, useState } from 'react';
-
-// import emailjs from 'emailjs-com';
+import emailjs from 'emailjs-com';
 import "./Contact.css";
 
-
-// const SERVICE_ID = 'service_3qy8lbc';
-// const TEMPLATE_ID = 'template_s6gfci4';
-// const USER_ID = 'RWSohpTYy2zdo_uXO';
-
+const SERVICE_ID = 'service_3qy8lbc';
+const TEMPLATE_ID = 'template_s6gfci4';
+const USER_ID = 'RWSohpTYy2zdo_uXO';
 
 const Contact = () => {
   const form = useRef();
 
   const [user, setUser] = useState("");
-  // const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
   const handleInputChange = (event) => {
     const x = document.querySelector(".validate");
     const y = document.querySelector(".invalidate");
+    const x1 = document.querySelector(".validate1");
+    const y1 = document.querySelector(".invalidate1");
     const x2 = document.querySelector(".validate2");
     const y2 = document.querySelector(".invalidate2");
+    const emailRegex = /^\S+@\S+\.\S+$/;
 
     const { name, value } = event.target;
-    
+
     if (name === "user_name") {
       console.log(value);
       setUser(value);
@@ -38,11 +38,25 @@ const Contact = () => {
         y.style.display = "block";
       }
     }
-    if (name === "message") {
+    if (name === "user_email") {
       console.log(value);
+      setEmail(value);
+      if (emailRegex.test(value)) {
+        console.log(value);
+        console.log('good');
+        x1.style.display = "block";
+        y1.style.display = "none";
+      } else {
+        console.log('no name');
+        x1.style.display = "none";
+        y1.style.display = "block";
+      }
+    }
+    if (name === "message") {
+      console.log(value.length);
       setMessage(value);
       if (value.length > 0) {
-        console.log(value.length);
+        console.log(value);
         console.log('good');
         x2.style.display = "block";
         y2.style.display = "none";
@@ -52,39 +66,23 @@ const Contact = () => {
         y2.style.display = "block";
       }
     }
-  }
+  };
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
-  //   // const emailRegex = /^\S+@\S+\.\S+$/;
-  //   // const validEmail = emailRegex.test(user_email) 
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  //   const user_name = e.target.user_name.value;
-  //   const message = e.target.message.value;
-  //   const user_email = e.target.user_email.value;
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      })
 
-  //   console.log(e.target.user_name.value);
-  //   console.log(e.target.message.value);
+    setMessage('');
+    setEmail('');
+    setUser('');
+  };
 
-  //   emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
-  //     .then((result) => {
-
-  //       if (user_name && message && user_email) {
-  //         console.log(result.text);
-  //         form.current.reset();
-  //       } else {
-  //         console.log('All fields must be filled.');
-  //         message.next().show();
-  //         return false;
-  //       }
-
-  //     }, (error) => {
-  //       console.log(error.text);
-  //     });
-  // }
- const sendEmail = () => {
-  console.log('email sent');
- }
   return (
 
     <div className="container-contactme">
@@ -100,9 +98,9 @@ const Contact = () => {
               value={user}
               name="user_name"
               placeholder="Name" />
-            <div className='validate' id='here'>
+            <div className='validate'>
               Looks good
-              <i className="fa-solid fa-check color-green"></i>
+              <i className="fa-solid fa-check"></i>
             </div>
             <div className='invalidate'>
               required
@@ -111,10 +109,19 @@ const Contact = () => {
             <label className="form-label">Email</label>
             <input
               className="form-control"
+              value={email}
               onChange={handleInputChange}
               type="email"
               name="user_email"
               placeholder="example@example.com" />
+            <div className='validate1'>
+              Looks good
+              <i className="fa-solid fa-check"></i>
+            </div>
+            <div className='invalidate1'>
+              required
+              <i className="fa-solid fa-check"></i>
+            </div>
             <label className="form-label">Message</label>
             <textarea
               className="form-control message"
@@ -124,7 +131,7 @@ const Contact = () => {
               placeholder="type your message..." />
             <div className='validate2'>
               Looks good
-              <i className="fa-solid fa-check color-green"></i>
+              <i className="fa-solid fa-check"></i>
             </div>
             <div className='invalidate2'>
               required
