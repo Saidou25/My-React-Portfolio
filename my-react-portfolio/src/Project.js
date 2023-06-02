cc56095ae1005e9cf3efc85ba138d65c4a2e223a
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-06-02 14:16:50 -0400

    some css

--- my-react-portfolio/src/Project.js
+++ my-react-portfolio/src/Project.js
@@ -10,7 +10,7 @@ function Project({ projects }) {
       <h3 className='title'>Projects</h3>
       <div className='container'>
         <div className='row'>
-          <div className='col-12 featured project'>
+          <div className='col-12 featured-project mt-5'>
             <div className='card'>
               <div href={'https://git.heroku.com/healing.git'} >
               </div>
