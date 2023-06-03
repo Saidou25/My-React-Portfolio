732139ce7aca42d9d31a9a470bacc90a49baeb33
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-06-03 12:18:20 -0400

    adding css

--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Project.css';
+import featuredImage from './healing.jpg';
+import logo from './logo.png';
+
 
 function Project({ projects }) {
 
@@ -9,12 +12,21 @@ function Project({ projects }) {
     <div>
       <h3 className='title'>Projects</h3>
       <div className='container'>
-        <div className='featured-project'>
-          <a href='https://healing.herokuapp.com/' >
-            Featured project
-          </a>
-        </div>
         <div className='row'>
+          <div className='col-12'>
+            <div className='card featured-card round'>
+              <img className="responsive" src={featuredImage} alt='website dashboard' />
+              <div className="card-img-overlay" id="text">
+                <a className='featured-project' href='https://healing.herokuapp.com/' >
+                  <h4 className="card-title">Healing</h4>
+                </a>
+                <a href='https://github.com/Saidou25/Healing'>
+                  <img className='logo' id="text" src={logo} alt="minicat" />
+                </a>
+              </div>
+            </div>
+
+          </div>
           {projects.map((project) => (
             <div className='col-lg-4 col-md-6 col-sm-12 flex effect' key={project.id}>
               <div className='card round'>
