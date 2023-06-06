d6394a768815404aefde7444c161c542613a9c81
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-06-06 07:49:33 -0400

    centered navbar on mobile view

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './Navigation.css';
 
-
 function Navigation({ currentPage, handlePageChange, pages }) {
 
   return (
-    // <div className='main-about'>
       <div className='row-top'>
         <div className='col-lg-4 myname'>
           <h1>Saidou Monta</h1>
         </div>
         <div className='col-lg-8 navigation'>
-          {/* <div className="navigation"> */}
             {pages.map((page) => (
               <article key={page} className='nav-item'>
                 <a
@@ -26,8 +23,6 @@ function Navigation({ currentPage, handlePageChange, pages }) {
             ))}
           </div>
         </div>
-      // </div>
-    //  </div>
   )
 };
 
