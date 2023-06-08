da98a57239176b308de4ee5bfe52e9840364d7f3
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-06-08 12:40:16 -0400

    fixed few typos in About

--- src/About.js
+++ src/About.js
@@ -19,16 +19,16 @@ function About() {
               Entry-level full stack web developer with proven problem solving, organization and research Skills.</p>
             <p className='story'>
               Native French I have been living in the US for over 15 years. Growing up training in
-              swiming at a national level I have developped competitivity and a hard working mentality.
-              My work journey started as a musician followed with the restautant industry in which
+              swimming at a national level I have developed competitiveness and a hard working mentality.
+              My work journey started as a musician followed with the restaurant industry in which
               I started in New York City as a dishwasher and climbed my way up to general manager.
             </p>
             <p className='story'>
-              Through my work experiences I have developped a lot of skills such as adaptability,
+              Through my work experiences I have developed a lot of skills such as adaptability,
               strong work ethic, team work, communication, attention to details, doing the "extra step" and many more.
             </p>
             <p className='story'>
-              Starting a new carreer and seeking an entry level full stack web developer role to pursue my new passion for building web sites, applications and grow with and within a conpany.
+              Starting a new career, highly motivated and seeking an entry level full stack web developer role to pursue my new passion for building web sites, applications and grow with and within a company.
             </p>
           </div>
         </div>
