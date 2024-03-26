44b9b4dca1506f6ac3032a3533a0386d40086312
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-03-26 07:45:59 -0400

    final without @mediascreen

--- src/About.js
+++ src/About.js
@@ -13,15 +13,13 @@ function About() {
           </div>
           <div className="col-lg-7 col-md-12 flex about-me">
             <p className="story">
-              As a native French speaker who has called the United States home
-              for over 15 years, I bring a unique blend of cultural perspectives
-              and experiences to the table. With a background in competitive
-              swimming at the national level, I've cultivated a spirit of
-              determination and a strong work ethic from a young age. My
-              professional journey has been diverse, initially beginning as a
-              musician and later transitioning into the restaurant industry in
-              the bustling heart of New York City. Starting as a dishwasher, I
-              tirelessly worked my way up the ranks to become a general manager.
+              With a background in competitive swimming at the national level,
+              I've cultivated a spirit of determination and a strong work ethic
+              from a young age. My professional journey has been diverse,
+              initially beginning as a jazz musician and later transitioning
+              into the restaurant industry in the bustling heart of New York
+              City. Starting as a dishwasher, I tirelessly worked my way up the
+              ranks to become general manager.
             </p>
             <p className="story">
               Throughout my diverse career path, I've honed a wide array of
@@ -30,13 +28,14 @@ function About() {
               detail, and a willingness to go the extra mile to achieve success.{" "}
             </p>
             <p className="story">
-              Driven by a newfound passion for web development, I am embarking
-              on a new career journey. I am highly motivated and eager to take
-              on an entry-level full-stack web developer role. My goal is to
-              leverage my problem-solving, organizational, and research skills
-              to contribute to web projects, build innovative websites and
-              applications, and continuously evolve as a professional within a
-              forward-thinking company.{" "}
+              Driven by my newfound passion for web development, I earned a full
+              Stack coding certificate from UNC Charlotte in North Carolina and
+              now embarking on a new career journey. Highly motivated and eager
+              to take on an entry-level front-end web developer role. My goal is
+              to leverage my problem solving, organizational, and research
+              skills to contribute to web projects, build innovative websites
+              and applications, and continuously evolve as a professional within
+              a forward-thinking company.
             </p>
             <p className="story">
               I am excited about the opportunities that lie ahead and look
